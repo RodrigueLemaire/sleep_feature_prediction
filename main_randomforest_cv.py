TimeSeries to fix data leakage

main_randomforest_cv.py
@@ -1,54 +1,121 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_validate, KFold
+from sklearn.model_selection import cross_validate, KFold, cross_val_predict
+from statsmodels.stats.diagnostic import acorr_ljungbox
+import numpy as np
 
 from datahelper import *
 
 # Settings
-days_window = 6
-columns_considered = ['average_breath', 'average_heart_rate', 'average_hrv',
+extra_days = 6
+input_features = ['average_breath', 'average_heart_rate', 'average_hrv',
          'deep_sleep_duration', 'light_sleep_duration', 'rem_sleep_duration']
-training_target = 'efficiency' #efficiency_tp1
+output_feature = 'n_correct'
 
-# Dataset processing
-df_list = []
+def training(
+        days_window:int,
+        columns_considered:list[str],
+        training_target:str):
 
-for ID in range(1, 30):
-    df = read_id(ID, data_sleep_all())
+    # Dataset processing
+    df_list = []
 
-    df = df[columns_considered + ["efficiency"]]
-    df["efficiency_tp1"] = df["efficiency"].copy().shift(-1)
+    for ID in range(1, 30):
+        # Extract sleep data and self-report data for a single user
+        data_sleep = read_id(ID, data_sleep_all())
+        data_selfreport = read_id(ID, data_selfreport_all())
 
+        # Merge both dataframes on date
+        data_sleep.rename(columns={'day': 'date'}, inplace=True)
+        data_all = data_sleep.merge(data_selfreport, on="date", how="left")
+        data_all.rename(columns={'id_x': 'id'}, inplace=True)
+        data_all.drop(columns=['id_y'], inplace=True)
+
+        # Keep date and total_sleep_duration (for indexing), then training columns only
+        df = data_all[
+            ["date", "total_sleep_duration"]
+            + columns_considered
+            + [training_target]]
+
+        # Convert date column to a DatetimeIndex for rolling averages
+        df.set_index(pd.to_datetime(df['date']), inplace=True)
+
+        # Remove duplicate index rows by keeping entries with the most total sleep
+        df = df.sort_values(by='total_sleep_duration', ascending=False)
+        df = df.drop_duplicates(subset='date', keep="first")
+
+        # Ensure no days are missing, data for missing days will be NaN
+        df = df.asfreq('D')
+
+        # Creation of new columns for previous day features
+        for days in range(1, days_window + 1):
+            for col in columns_considered:
+                df[col + "_tm" + str(days)] = df[col].copy().shift(days)
+
+        # Drop all rows still containing NaN values
+        df = df.dropna()
+
+        df_list.append(df)
+
+    # Combine all participants' dataframe into one for training
+    data = pd.concat(df_list, ignore_index=True)
+
+    # Model training
+    training_columns = columns_considered.copy()
     for days in range(1, days_window + 1):
         for col in columns_considered:
-            df[col + "_tm" + str(days)] = df[col].copy().shift(days)
+            training_columns.append(col + "_tm" + str(days))
 
-    df = df.dropna()
+    X = data[training_columns] # Data over the previous days
+    y = data[training_target] # Sleep efficiency for the next day
 
-    df_list.append(df)
+    rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
 
-data = pd.concat(df_list, ignore_index=True)
+    cv = KFold(n_splits=10, shuffle=True, random_state=42)
+    scores = cross_validate(
+        rf_regressor, X, y, cv=cv,
+        scoring=['neg_mean_absolute_error', 'neg_mean_squared_error', 'r2'],
+        return_estimator=True,
+    )
 
-# Model training
-training_columns = columns_considered.copy()
-for days in range(1, days_window + 1):
-    for col in columns_considered:
-        training_columns.append(col + "_tm" + str(days))
+    mae = -scores['test_neg_mean_absolute_error'].mean()
+    mse = -scores['test_neg_mean_squared_error'].mean()
+    r2 = scores['test_r2'].mean()
 
-X = data[training_columns] # Data over the previous days
-y = data[training_target] # Sleep efficiency for the next day
+    # Check for autocorrelation
+    y_pred_oof = cross_val_predict(rf_regressor, X, y, cv=cv, n_jobs=-1)
 
-rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+    # Cross-validated residuals
+    y = pd.to_numeric(y, errors="raise").to_numpy()
+    residuals = y - y_pred_oof
 
-cv = KFold(n_splits=5, shuffle=True, random_state=42)
-scores = cross_validate(
-    rf_regressor, X, y, cv=cv,
-    scoring=['neg_mean_absolute_error', 'neg_mean_squared_error', 'r2']
-)
+    # Collect feature importances from each fold
+    importances = np.vstack([
+        est.feature_importances_ for est in scores["estimator"]
+    ])
 
-mae = -scores['test_neg_mean_absolute_error'].mean()
-mse = -scores['test_neg_mean_squared_error'].mean()
-r2 = scores['test_r2'].mean()
+    # Mean and std across folds
+    mean_importances = importances.mean(axis=0)
 
-print(f"Mean Absolute Error: {mae:.2f}")
-print(f"Mean Squared Error: {mse:.2f}")
-print(f"R-squared Score: {r2:.2f}")
+    importance_df = pd.DataFrame({
+        "feature": training_columns,
+        "mean_importance": mean_importances,
+    }).sort_values('feature', ascending=False)
+
+    return mae, mse, r2, importance_df, residuals
+
+if __name__ == "__main__":
+
+    print(f"\n------------ RESULTS FOR DAY {extra_days} ------------")
+    (mae, mse, r2, importance, residuals) \
+        = training(extra_days, input_features, output_feature)
+
+
+    print(f"Mean Absolute Error: {mae:.2f}")
+    print(f"Mean Squared Error: {mse:.2f}")
+    print(f"R-squared Score: {r2:.2f}")
+
+    print(f"Feature Importance: \n{importance}")
+
+    lags = min(10, len(residuals) // 5)
+    lb = acorr_ljungbox(residuals, lags=[lags], return_df=True)
+    print(f"\nLjung-Box test: \n{lb}")

main_randomforest_cv_TS_consecutive.py
@@ -1,12 +1,14 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_validate, LeaveOneGroupOut, cross_val_predict
+from sklearn.model_selection import cross_validate, TimeSeriesSplit, cross_val_predict
+from sklearn.dummy import DummyRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from statsmodels.stats.diagnostic import acorr_ljungbox
 import numpy as np
 
 from datahelper import *
 
 # Settings
-extra_days = 6
+extra_days = 30
 input_features = ['average_breath', 'average_heart_rate', 'average_hrv',
          'deep_sleep_duration', 'light_sleep_duration', 'rem_sleep_duration']
 output_feature = 'n_correct'
@@ -14,7 +16,8 @@ output_feature = 'n_correct'
 def training(
         days_window:int,
         columns_considered:list[str],
-            training_target:str):
+        training_target:str):
+
     # Dataset processing
     df_list = []
     missing_entries = []
@@ -30,9 +33,9 @@ def training(
         data_all.rename(columns={'id_x': 'id'}, inplace=True)
         data_all.drop(columns=['id_y'], inplace=True)
 
-        # Keep date and total_sleep_duration (for indexing), id for LOGO, then training columns only
+        # Keep date and total_sleep_duration (for indexing), then training columns only
         df = data_all[
-            ["date", "total_sleep_duration", "id"]
+            ["date", "total_sleep_duration"]
             + columns_considered
             + [training_target]]
 
@@ -62,6 +65,7 @@ def training(
         else:
             missing_entries.append([0])
 
+        # Drop all rows still containing NaN values
         df = df.dropna()
 
         df_list.append(df)
@@ -71,6 +75,7 @@ def training(
 
     # Combine all participants' dataframe into one for training
     data = pd.concat(df_list, ignore_index=True)
+    data = data.sort_values(by='date')
 
     # Model training
     training_columns = columns_considered.copy()
@@ -83,45 +88,64 @@ def training(
 
     rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
 
-    cv = LeaveOneGroupOut()
+    cv = TimeSeriesSplit(n_splits=10, gap=days_window)
     scores = cross_validate(
-        rf_regressor,
-        X, y,
-        cv=cv,
-        groups=data["id"],   # divide by participant
+        rf_regressor, X, y, cv=cv,
         scoring=['neg_mean_absolute_error', 'neg_mean_squared_error', 'r2'],
         return_estimator=True,
     )
 
-    mae = -scores['test_neg_mean_absolute_error']
-    mse = -scores['test_neg_mean_squared_error']
-    r2 = scores['test_r2']
+    mae = -scores['test_neg_mean_absolute_error'].mean()
+    mse = -scores['test_neg_mean_squared_error'].mean()
+    r2 = scores['test_r2'].mean()
 
-    # Out-of-group (leave-one-participant-out) predictions
-    y_pred_logo = cross_val_predict(
-        rf_regressor, X, y,
-        groups=data["id"],
-        cv=cv,
-        n_jobs=-1
-    )
+    # Check for autocorrelation
+    #y_pred_oof = cross_val_predict(rf_regressor, X, y, cv=cv, n_jobs=-1)
 
-    # Residuals for all rows (still in long format)
-    residuals = y.astype(float) - y_pred_logo
+    # Cross-validated residuals
+    #y = pd.to_numeric(y, errors="raise").to_numpy()
+    #residuals = y - y_pred_oof
 
-    return missing_entries, mae, mse, r2, residuals
+    # Collect feature importances from each fold
+    importances =  np.vstack([
+        est.feature_importances_ for est in scores["estimator"]
+    ])
+
+    # Mean and std across folds
+    mean_importances = importances.mean(axis=0)
+
+    importance_df = pd.DataFrame({
+        "feature": training_columns,
+        "mean_importance": mean_importances,
+    }).sort_values('feature', ascending=False)
+
+    # Dummy regressor with median strategy
+    dummy = DummyRegressor(strategy="median")
+    dummy.fit(X, y)
+
+    y_pred_dummy = dummy.predict(X)
+
+    mae_dummy = mean_absolute_error(y, y_pred_dummy)
+    mse_dummy = mean_squared_error(y, y_pred_dummy)
+    r2_dummy = r2_score(y, y_pred_dummy)
+
+    return (missing_entries, mae, mae_dummy, mse,
+            mse_dummy, r2, r2_dummy, importance_df)
+
 
 if __name__ == "__main__":
 
-    print(f"\n------------ RESULTS FOR {extra_days+1} DAYS ------------")
-    (missingness, mae, mse, r2, residuals) \
-        = training(extra_days, input_features, output_feature)
+    for days in range(0, extra_days):
+        print(f"\n------------ RESULTS FOR {days+1} DAYS ------------")
+        (missingness, mae, mae_dummy, mse, mse_dummy, r2, r2_dummy, importance) \
+            = training(days, input_features, output_feature)
 
-    print(f"Missing entries: {missingness:.2f}%")
+        print(f"Missing entries: {missingness:.2f}%")
 
-    print(f"Mean Absolute Error: {mae}")
-    print(f"Mean Squared Error: {mse}")
-    print(f"R-squared Score: {r2}")
+        print(f"Mean Absolute Error (vs. Dummy): {mae:.2f} / {mae_dummy:.2f}")
+        print(f"Mean Squared Error (vs. Dummy): {mse:.2f} / {mse_dummy:.2f}")
+        print(f"R-squared Score (vs. Dummy): {r2:.2f} / {r2_dummy:.2f}")
 
-    lags = min(10, len(residuals) // 5)
-    lb = acorr_ljungbox(residuals, lags=[lags], return_df=True)
-    print(f"\nLjung-Box test: \n{lb}")
+        print(f"Feature Importance: \n{importance}")
+
+        #np.savetxt('residuals.txt', residuals, delimiter=',')

main_randomforest_cv_consecutive.py
@@ -1,124 +1,154 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_validate, KFold
+from sklearn.model_selection import cross_validate, KFold, cross_val_predict
 from sklearn.dummy import DummyRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from statsmodels.stats.diagnostic import acorr_ljungbox
 import numpy as np
 
 from datahelper import *
 
 # Settings
-days_window = 6     # WARNING: Total window is equal to 1 + days_window
-columns_considered = ['average_breath', 'average_heart_rate', 'average_hrv',
+extra_days = 1
+input_features = ['average_breath', 'average_heart_rate', 'average_hrv',
          'deep_sleep_duration', 'light_sleep_duration', 'rem_sleep_duration']
-training_target = 'n_correct'
+output_feature = 'n_correct'
 
-# Dataset processing
-df_list = []
-missing_entries = []
+def training(
+        days_window:int,
+        columns_considered:list[str],
+        training_target:str):
 
-for ID in range(1, 30):
-    # Extract sleep data and self-report data for a single user
-    data_sleep = read_id(ID, data_sleep_all())
-    data_selfreport = read_id(ID, data_selfreport_all())
+    # Dataset processing
+    df_list = []
+    missing_entries = []
 
-    # Merge both dataframes on date
-    data_sleep.rename(columns={'day': 'date'}, inplace=True)
-    data_all = data_sleep.merge(data_selfreport, on="date", how="left")
-    data_all.rename(columns={'id_x': 'id'}, inplace=True)
-    data_all.drop(columns=['id_y'], inplace=True)
+    for ID in range(1, 30):
+        # Extract sleep data and self-report data for a single user
+        data_sleep = read_id(ID, data_sleep_all())
+        data_selfreport = read_id(ID, data_selfreport_all())
 
-    # Keep date and total_sleep_duration (for indexing), then training columns only
-    df = data_all[
-        ["date", "total_sleep_duration"]
-        + columns_considered
-        + [training_target]]
+        # Merge both dataframes on date
+        data_sleep.rename(columns={'day': 'date'}, inplace=True)
+        data_all = data_sleep.merge(data_selfreport, on="date", how="left")
+        data_all.rename(columns={'id_x': 'id'}, inplace=True)
+        data_all.drop(columns=['id_y'], inplace=True)
 
-    # Convert date column to a DatetimeIndex for rolling averages
-    df.set_index(pd.to_datetime(df['date']), inplace=True)
+        # Keep date and total_sleep_duration (for indexing), then training columns only
+        df = data_all[
+            ["date", "total_sleep_duration"]
+            + columns_considered
+            + [training_target]]
 
-    # Remove duplicate index rows by keeping entries with the most total sleep
-    df = df.sort_values(by='total_sleep_duration', ascending=False)
-    df = df.drop_duplicates(subset='date', keep="first")
+        # Convert date column to a DatetimeIndex for rolling averages
+        df.set_index(pd.to_datetime(df['date']), inplace=True)
 
-    #df = df[~df.index.duplicated(keep='first')]
+        # Remove duplicate index rows by keeping entries with the most total sleep
+        df = df.sort_values(by='total_sleep_duration', ascending=False)
+        df = df.drop_duplicates(subset='date', keep="first")
 
-    # Ensure no days are missing, data for missing days will be NaN
-    df = df.asfreq('D')
+        # Ensure no days are missing, data for missing days will be NaN
+        df = df.asfreq('D')
 
+        if days_window > 0:
+            for col in columns_considered:
+                # Calculate rolling window averages in a new column
+                df[col + "_average"] = (
+                    df[col].shift(1).rolling(str(days_window) + 'D', min_periods=1).mean())
+
+                # Count NaN amount in rolling window
+                missing_entries.append(
+                    (
+                        days_window -
+                        df[col].shift(1).rolling(str(days_window) + 'D', min_periods=0).count()
+                    ).mean()
+                )
+        else:
+            missing_entries.append([0])
+
+        # Drop all rows still containing NaN values
+        df = df.dropna()
+
+        df_list.append(df)
+
+    # Compute final missingness
+    missing_entries = np.mean(missing_entries)
+
+    # Combine all participants' dataframe into one for training
+    data = pd.concat(df_list, ignore_index=True)
+
+    # Model training
+    training_columns = columns_considered.copy()
     if days_window > 0:
         for col in columns_considered:
-            # Calculate rolling window averages in a new column
-            df[col + "_average"] = (
-                df[col].shift(1).rolling(str(days_window) + 'D', min_periods=1).mean())
+            training_columns.append(col + "_average")
 
-            # Count NaN amount in rolling window
-            missing_entries.append(
-                (
-                    days_window -
-                    df[col].shift(1).rolling(str(days_window) + 'D', min_periods=0).count()
-                ).mean()
-            )
+    X = data[training_columns] # Data over the previous days
+    y = data[training_target] # Sleep efficiency for the next day
 
-    # Drop all rows still containing NaN values
-    df = df.dropna()
+    rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
 
-    df_list.append(df)
+    cv = KFold(n_splits=10, shuffle=True, random_state=42)
+    scores = cross_validate(
+        rf_regressor, X, y, cv=cv,
+        scoring=['neg_mean_absolute_error', 'neg_mean_squared_error', 'r2'],
+        return_estimator=True,
+    )
 
-# Compute final missingness
-missing_entries = np.mean(missing_entries)
+    mae = -scores['test_neg_mean_absolute_error'].mean()
+    mse = -scores['test_neg_mean_squared_error'].mean()
+    r2 = scores['test_r2'].mean()
 
-# Combine all participants' dataframe into one for training
-data = pd.concat(df_list, ignore_index=True)
+    # Check for autocorrelation
+    y_pred_oof = cross_val_predict(rf_regressor, X, y, cv=cv, n_jobs=-1)
 
-# Model training
-training_columns = columns_considered.copy()
-if days_window > 0:
-    for col in columns_considered:
-        training_columns.append(col + "_average")
+    # Cross-validated residuals
+    y = pd.to_numeric(y, errors="raise").to_numpy()
+    residuals = y - y_pred_oof
 
-X = data[training_columns] # Data over the previous days
-y = data[training_target] # Sleep efficiency for the next day
+    # Collect feature importances from each fold
+    importances =  np.vstack([
+        est.feature_importances_ for est in scores["estimator"]
+    ])
 
-rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+    # Mean and std across folds
+    mean_importances = importances.mean(axis=0)
 
-cv = KFold(n_splits=10, shuffle=True, random_state=42)
-scores = cross_validate(
-    rf_regressor, X, y, cv=cv,
-    scoring=['neg_mean_absolute_error', 'neg_mean_squared_error', 'r2'],
-    return_estimator=True,
-)
+    importance_df = pd.DataFrame({
+        "feature": training_columns,
+        "mean_importance": mean_importances,
+    }).sort_values('feature', ascending=False)
 
-mae = -scores['test_neg_mean_absolute_error'].mean()
-mse = -scores['test_neg_mean_squared_error'].mean()
-r2 = scores['test_r2'].mean()
+    # Dummy regressor with median strategy
+    dummy = DummyRegressor(strategy="median")
+    dummy.fit(X, y)
 
-# Collect feature importances from each fold
-importances =  np.vstack([
-    est.feature_importances_ for est in scores["estimator"]
-])
+    y_pred_dummy = dummy.predict(X)
 
-# Mean and std across folds
-mean_importances = importances.mean(axis=0)
+    mae_dummy = mean_absolute_error(y, y_pred_dummy)
+    mse_dummy = mean_squared_error(y, y_pred_dummy)
+    r2_dummy = r2_score(y, y_pred_dummy)
 
-importance_df = pd.DataFrame({
-    "feature": training_columns,
-    "mean_importance": mean_importances,
-}).sort_values('feature', ascending=False)
+    return (missing_entries, mae, mae_dummy, mse,
+            mse_dummy, r2, r2_dummy, importance_df, residuals)
 
-# Dummy regressor with median strategy
-dummy = DummyRegressor(strategy="median")
-dummy.fit(X, y)
 
-y_pred_dummy = dummy.predict(X)
+if __name__ == "__main__":
 
-mae_dummy = mean_absolute_error(y, y_pred_dummy)
-mse_dummy = mean_squared_error(y, y_pred_dummy)
-r2_dummy = r2_score(y, y_pred_dummy)
+    for days in range(0, extra_days):
+        print(f"\n------------ RESULTS FOR {days+1} DAYS ------------")
+        (missingness, mae, mae_dummy, mse, mse_dummy, r2, r2_dummy, importance, residuals) \
+            = training(days, input_features, output_feature)
 
-print(f"Missing entries: {missing_entries:.2f}%")
+        print(f"Missing entries: {missingness:.2f}%")
 
-print(f"Mean Absolute Error (vs. Dummy): {mae:.2f} / {mae_dummy:.2f}")
-print(f"Mean Squared Error (vs. Dummy): {mse:.2f} / {mse_dummy:.2f}")
-print(f"R-squared Score (vs. Dummy): {r2:.2f} / {r2_dummy:.2f}")
+        print(f"Mean Absolute Error (vs. Dummy): {mae:.2f} / {mae_dummy:.2f}")
+        print(f"Mean Squared Error (vs. Dummy): {mse:.2f} / {mse_dummy:.2f}")
+        print(f"R-squared Score (vs. Dummy): {r2:.2f} / {r2_dummy:.2f}")
 
-print(f"Feature Importance: \n{importance_df}")
+        print(f"Feature Importance: \n{importance}")
+
+        lags = min(10, len(residuals) // 5)
+        lb = acorr_ljungbox(residuals, lags=[lags], return_df=True)
+        print(f"\nLjung-Box test: \n{lb}")
+
+    #np.savetxt('residuals.txt', residuals, delimiter=',')

main_randomforest_cv_consecutive_atemporal.py
@@ -1,104 +1,129 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_validate, KFold
+from sklearn.model_selection import cross_validate, KFold, cross_val_predict
 from sklearn.dummy import DummyRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from statsmodels.stats.diagnostic import acorr_ljungbox
 import numpy as np
 
 from datahelper import *
 
 # Settings
-columns_considered = ['average_breath', 'average_heart_rate', 'average_hrv',
+input_features = ['average_breath', 'average_heart_rate', 'average_hrv',
          'deep_sleep_duration', 'light_sleep_duration', 'rem_sleep_duration']
-training_target = 'n_correct'
+output_feature = 'n_correct'
 
-# Dataset processing
-df_list = []
+def training(
+        columns_considered:list[str],
+        training_target:str):
 
-for ID in range(1, 30):
-    # Extract sleep data and self-report data for a single user
-    data_sleep = read_id(ID, data_sleep_all())
-    data_selfreport = read_id(ID, data_selfreport_all())
+    # Dataset processing
+    df_list = []
 
-    # Merge both dataframes on date
-    data_sleep.rename(columns={'day': 'date'}, inplace=True)
-    data_all = data_sleep.merge(data_selfreport, on="date", how="left")
-    data_all.rename(columns={'id_x': 'id'}, inplace=True)
-    data_all.drop(columns=['id_y'], inplace=True)
+    for ID in range(1, 30):
+        # Extract sleep data and self-report data for a single user
+        data_sleep = read_id(ID, data_sleep_all())
+        data_selfreport = read_id(ID, data_selfreport_all())
 
-    # Keep date and total_sleep_duration (for indexing), then training columns only
-    df = data_all[
-        ["date", "total_sleep_duration"]
-        + columns_considered
-        + [training_target]]
+        # Merge both dataframes on date
+        data_sleep.rename(columns={'day': 'date'}, inplace=True)
+        data_all = data_sleep.merge(data_selfreport, on="date", how="left")
+        data_all.rename(columns={'id_x': 'id'}, inplace=True)
+        data_all.drop(columns=['id_y'], inplace=True)
 
-    # Convert date column to a DatetimeIndex for rolling averages
-    df.set_index(pd.to_datetime(df['date']), inplace=True)
+        # Keep date and total_sleep_duration (for indexing), then training columns only
+        df = data_all[
+            ["date", "total_sleep_duration"]
+            + columns_considered
+            + [training_target]]
 
-    # Remove duplicate index rows by keeping entries with the most total sleep
-    df = df.sort_values(by='total_sleep_duration', ascending=False)
-    df = df.drop_duplicates(subset='date', keep="first")
+        # Convert date column to a DatetimeIndex for rolling averages
+        df.set_index(pd.to_datetime(df['date']), inplace=True)
 
-    #df = df[~df.index.duplicated(keep='first')]
+        # Remove duplicate index rows by keeping entries with the most total sleep
+        df = df.sort_values(by='total_sleep_duration', ascending=False)
+        df = df.drop_duplicates(subset='date', keep="first")
 
-    # Ensure no days are missing, data for missing days will be NaN
-    df = df.asfreq('D')
+        #df = df[~df.index.duplicated(keep='first')]
 
+        # Ensure no days are missing, data for missing days will be NaN
+        df = df.asfreq('D')
+
+        for col in columns_considered:
+            df[col + "_average"] = df[col].shift(1).shift(1).expanding().mean()
+
+        df = df.dropna()
+
+        df_list.append(df)
+
+    # Combine all participants' dataframe into one for training
+    data = pd.concat(df_list, ignore_index=True)
+
+    # Model training
+    training_columns = columns_considered.copy()
     for col in columns_considered:
-        df[col + "_average"] = df[col].shift(1).shift(1).expanding().mean()
+        training_columns.append(col + "_average")
 
-    df = df.dropna()
+    X = data[training_columns] # Data over the previous days
+    y = data[training_target] # Sleep efficiency for the next day
 
-    df_list.append(df)
+    rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
 
-# Combine all participants' dataframe into one for training
-data = pd.concat(df_list, ignore_index=True)
+    cv = KFold(n_splits=10, shuffle=True, random_state=42)
+    scores = cross_validate(
+        rf_regressor, X, y, cv=cv,
+        scoring=['neg_mean_absolute_error', 'neg_mean_squared_error', 'r2'],
+        return_estimator=True,
+    )
 
-# Model training
-training_columns = columns_considered.copy()
-for col in columns_considered:
-    training_columns.append(col + "_average")
+    mae = -scores['test_neg_mean_absolute_error'].mean()
+    mse = -scores['test_neg_mean_squared_error'].mean()
+    r2 = scores['test_r2'].mean()
 
-X = data[training_columns] # Data over the previous days
-y = data[training_target] # Sleep efficiency for the next day
+    # Check for autocorrelation
+    y_pred_oof = cross_val_predict(rf_regressor, X, y, cv=cv, n_jobs=-1)
 
-rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+    # Cross-validated residuals
+    y = pd.to_numeric(y, errors="raise").to_numpy()
+    residuals = y - y_pred_oof
 
-cv = KFold(n_splits=10, shuffle=True, random_state=42)
-scores = cross_validate(
-    rf_regressor, X, y, cv=cv,
-    scoring=['neg_mean_absolute_error', 'neg_mean_squared_error', 'r2'],
-    return_estimator=True,
-)
+    # Collect feature importances from each fold
+    importances =  np.vstack([
+        est.feature_importances_ for est in scores["estimator"]
+    ])
 
-mae = -scores['test_neg_mean_absolute_error'].mean()
-mse = -scores['test_neg_mean_squared_error'].mean()
-r2 = scores['test_r2'].mean()
+    # Mean and std across folds
+    mean_importances = importances.mean(axis=0)
 
-# Collect feature importances from each fold
-importances =  np.vstack([
-    est.feature_importances_ for est in scores["estimator"]
-])
+    importance_df = pd.DataFrame({
+        "feature": training_columns,
+        "mean_importance": mean_importances,
+    }).sort_values('feature', ascending=False)
 
-# Mean and std across folds
-mean_importances = importances.mean(axis=0)
+    # Dummy regressor with median strategy
+    dummy = DummyRegressor(strategy="median")
+    dummy.fit(X, y)
 
-importance_df = pd.DataFrame({
-    "feature": training_columns,
-    "mean_importance": mean_importances,
-}).sort_values('feature', ascending=False)
+    y_pred_dummy = dummy.predict(X)
 
-# Dummy regressor with median strategy
-dummy = DummyRegressor(strategy="median")
-dummy.fit(X, y)
+    mae_dummy = mean_absolute_error(y, y_pred_dummy)
+    mse_dummy = mean_squared_error(y, y_pred_dummy)
+    r2_dummy = r2_score(y, y_pred_dummy)
 
-y_pred_dummy = dummy.predict(X)
+    return mae, mae_dummy, mse, mse_dummy, r2, r2_dummy, importance_df, residuals
 
-mae_dummy = mean_absolute_error(y, y_pred_dummy)
-mse_dummy = mean_squared_error(y, y_pred_dummy)
-r2_dummy = r2_score(y, y_pred_dummy)
 
-print(f"Mean Absolute Error (vs. Dummy): {mae:.2f} / {mae_dummy:.2f}")
-print(f"Mean Squared Error (vs. Dummy): {mse:.2f} / {mse_dummy:.2f}")
-print(f"R-squared Score (vs. Dummy): {r2:.2f} / {r2_dummy:.2f}")
+if __name__ == "__main__":
 
-print(f"Feature Importance: \n{importance_df}")
+    print(f"\n------------- RESULTS FOR ALL DAYS -------------")
+    (mae, mae_dummy, mse, mse_dummy, r2, r2_dummy, importance, residuals) \
+        = training(input_features, output_feature)
+
+    print(f"Mean Absolute Error (vs. Dummy): {mae:.2f} / {mae_dummy:.2f}")
+    print(f"Mean Squared Error (vs. Dummy): {mse:.2f} / {mse_dummy:.2f}")
+    print(f"R-squared Score (vs. Dummy): {r2:.2f} / {r2_dummy:.2f}")
+
+    print(f"Feature Importance: \n{importance}")
+
+    lags = min(10, len(residuals) // 5)
+    lb = acorr_ljungbox(residuals, lags=[lags], return_df=True)
+    print(f"\nLjung-Box test: \n{lb}")

main_stats.py
@@ -6,7 +6,7 @@ from scipy.stats import spearmanr
 
 from datahelper import *
 
-TESTING = 8
+TESTING = 1
 if __name__ == "__main__":
     logging.debug("start_main\n")
 